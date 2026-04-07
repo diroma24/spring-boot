b981e9252a9953a248f65fda258f1f401ca82146
PortatilNuevo <portail@nuevo.com>
2026-04-07 10:07:39 +0200

    Cambiada la raiz a la carpeta Spring-boot

---
+++ minima/src/main/java/es/romadie/da2d1a/minima/MinimaApplication.java
@@ -0,0 +1,13 @@
+package es.romadie.da2d1a.minima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimaApplication.class, args);
+    }
+
+}
